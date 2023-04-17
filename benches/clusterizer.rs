c115cc42afa867b1b747ecd7ac5c83d8d4f578bf
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-04-17 21:48:04 +0000

    Exercises

--- benches/clusterizer.rs
+++ benches/clusterizer.rs
@@ -13,4 +13,15 @@ use rand_xoshiro::Xoroshiro128PlusPlus;
 pub fn bench_clusterize(c: &mut Criterion) {
     let mut group = c.benchmark_group("Clusterizer::clusterize");
 
-    group.warm_up_time(Duration::from_
+    group.warm_up_time(Duration::from_secs(5));
+    group.sample_size(1000);
+    group.measurement_time(Duration::from_secs(15));
+
+    let init = Square::new(100, 100, 100).into();
+
+    for n in [10, 30, 50].iter() {
+        let id = BenchmarkId::from_parameter(n);
+
+        group.throughput(Throughput::Elements(*n as u64));
+
+ 
