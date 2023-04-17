f67bd16ece000d59766a5d503af7199ae94a4a5c
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-04-17 05:26:12 +0000

    shh

--- benches/clusterizer.rs
+++ benches/clusterizer.rs
@@ -7,4 +7,10 @@ use criterion::{black_box, BenchmarkId, Criterion, Throughput};
 
 use rand::prelude::*;
 
-use pico_detect:
+use pico_detect::{clusterize::Clusterizer, perturbate::Perturbator, Detection, Square};
+use rand_xoshiro::Xoroshiro128PlusPlus;
+
+pub fn bench_clusterize(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Clusterizer::clusterize");
+
+    group.warm_up_time(Duration::from_
