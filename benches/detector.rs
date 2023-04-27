3912f21ed79146f7b4e54b697dc19eeafe1b8d7e
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-04-27 22:47:05 +0000

    Update docu

--- benches/detector.rs
+++ benches/detector.rs
@@ -3,4 +3,13 @@ mod macros;
 
 use std::fs;
 
-use cr
+use criterion::{black_box, Criterion};
+
+use image;
+use pico_detect::{Detector, Square};
+
+pub fn bench_load(c: &mut Criterion) {
+    let model_data = fs::read(model_path!(facefinder)).unwrap();
+
+    c.bench_function("Detector::load", |b| {
+        b.iter(|| Detector::load(black_box(m
