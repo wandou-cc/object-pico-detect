984c5ae887ac551c594384f6a31a570de0ce95d2
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-05-21 13:45:21 +0000

    change tagline

--- benches/shaper.rs
+++ benches/shaper.rs
@@ -6,4 +6,14 @@ use std::fs;
 use criterion::{black_box, Criterion};
 
 use image;
-use pico_detect::{Shaper
+use pico_detect::{Shaper, Square};
+
+pub fn bench_load(c: &mut Criterion) {
+    let model_data = fs::read(model_path!(shaper)).unwrap();
+
+    c.bench_function("Shaper::load", |b| {
+        b.iter(|| Shaper::load(black_box(model_data.as_slice())).unwrap())
+    });
+}
+
+pub fn bench_inference(c: &mut Crit
