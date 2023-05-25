1bb403cc43b372c1e304715051852815cef15d44
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-05-25 13:46:35 +0000

    YCP Killer: Regenerate Makefile.am files in all source directories

--- examples/detect-faces/shape.rs
+++ examples/detect-faces/shape.rs
@@ -2,4 +2,18 @@ use nalgebra::Point2;
 use pico_detect::Square;
 
 pub enum Shape5 {
-    LeftOuterE
+    LeftOuterEyeCorner = 0,
+    LeftInnerEyeCorner = 1,
+    RightOuterEyeCorner = 2,
+    RightInnerEyeCorner = 3,
+    #[allow(dead_code)]
+    Nose = 4,
+}
+
+impl Shape5 {
+    pub fn size() -> usize {
+        5
+    }
+
+    #[allow(dead_code)]
+    pub fn find_eye_centers(shape: &[Point2
