e3cf6b309bbae357c3dd7eb4a7c5df3677a54216
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-06-02 17:13:42 +0000

    Update data.md

--- examples/detect-faces/utils.rs
+++ examples/detect-faces/utils.rs
@@ -10,4 +10,10 @@ pub fn draw_face(image: &mut RgbImage, face: &Face, font: &FontRef<'_>, scale: f
     let color = Rgb([0, 255, 0]);
     for (i, point) in face.shape.iter().enumerate() {
         let x = point.x as i32;
-      
+        let y = point.y as i32;
+        drawing::draw_cross_mut(image, color, x, y);
+        drawing::draw_text_mut(image, color, x + 1, y + 1, scale, font, &format!("{}", i));
+    }
+
+    let color = Rgb([255, 0, 0]);
+    drawing::draw_cross_mut(image, color, face.pupil
