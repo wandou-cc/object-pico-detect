13b2ed52db1a64639c117788e67d4e0e751e3b4e
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-06-02 14:52:02 +0000

    [Gradle Release Plugin] - pre tag commit:  '1.0'.

--- examples/detect-faces/utils.rs
+++ examples/detect-faces/utils.rs
@@ -4,4 +4,10 @@ use imageproc::drawing;
 
 use crate::face::Face;
 
-pub fn draw_fac
+pub fn draw_face(image: &mut RgbImage, face: &Face, font: &FontRef<'_>, scale: f32) {
+    drawing::draw_hollow_rect_mut(image, face.region.into(), Rgb([0, 0, 255]));
+
+    let color = Rgb([0, 255, 0]);
+    for (i, point) in face.shape.iter().enumerate() {
+        let x = point.x as i32;
+      
