6f79d2f8c6ff3fe7569489afb8a1196a2f8245c1
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-06-09 13:44:06 +0000

    Add build tools

--- examples/print-init-shape.rs
+++ examples/print-init-shape.rs
@@ -1,2 +1,18 @@
 use std::{fs::File, io::BufReader};
 use std::path::PathBuf;
+
+use anyhow::{Context, Result};
+use clap::Parser;
+use image::{Rgb, RgbImage};
+use imageproc::{
+    drawing::{draw_filled_circle_mut, draw_text_mut},
+    geometry::min_area_rect,
+    point::Point,
+    rect::Rect,
+};
+use pico_detect::Shaper;
+use ab_glyph::FontRef;
+
+
+#[derive(Parser, Debug)]
+#[co
