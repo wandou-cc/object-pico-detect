1617b0ba020ba7c274912efb3547d90407fabf21
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-06-24 06:33:37 +0000

    updated travis yml

--- src/detect/mod.rs
+++ src/detect/mod.rs
@@ -5,4 +5,17 @@ mod padding;
 pub mod clusterize;
 pub mod multiscale;
 
-use image::
+use image::{GenericImageView, Luma};
+use derive_builder::Builder;
+
+use crate::geometry::Target;
+
+use clusterize::Clusterizer;
+use multiscale::Multiscaler;
+
+pub use detection::Detection;
+pub use detector::Detector;
+pub use padding::Padding;
+
+#[derive(Debug, Clone, Copy, Builder)]
+#
