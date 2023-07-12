331e4ec9d18f8c32d6b9aa629e11b9034876ef81
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-07-12 10:34:00 +0000

    0.1.28

--- src/detect/multiscale.rs
+++ src/detect/multiscale.rs
@@ -8,4 +8,14 @@ pub struct Multiscaler {
     min_size: u32,
     max_size: u32,
     shift_factor: f32,
-    scale_factor: 
+    scale_factor: f32,
+}
+
+#[derive(Debug, Error)]
+pub enum MultiscalerError {
+    #[error("`min_size` should be non zero")]
+    MinSizeIsZero,
+    #[error("`max_size` should be greater than `min_size`")]
+    MaxSizeLessThanMinSize,
+    #[error("`shift_factor` should be in `(0, 1]` range")]
+  
