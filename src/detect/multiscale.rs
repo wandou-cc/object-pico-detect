57ad63d8e51891403df3d2050d1f781fe46b7989
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-07-15 14:21:25 +0000

    ALSA: hda - Add fixup for ASUS N56VZ

--- src/detect/multiscale.rs
+++ src/detect/multiscale.rs
@@ -18,4 +18,16 @@ pub enum MultiscalerError {
     #[error("`max_size` should be greater than `min_size`")]
     MaxSizeLessThanMinSize,
     #[error("`shift_factor` should be in `(0, 1]` range")]
-  
+    ShiftFactorOutOfRange,
+    #[error("`scale_factor` should be greater than 1")]
+    ScaleFactorLessThanOne,
+}
+
+impl Multiscaler {
+    #[inline]
+    pub fn new(
+        min_size: u32,
+        max_size: u32,
+        shift_factor: f32,
+        scale_factor: f32,
+    ) -> Res
