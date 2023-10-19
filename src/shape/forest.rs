04ddfb871a0e3624f39554c4efac9a2bbb245986
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-10-19 15:33:10 +0000

    efi: Add pstore variables to the deletion whitelist

--- src/shape/forest.rs
+++ src/shape/forest.rs
@@ -1,4 +1,14 @@
 use core::mem::size_of;
 use std::io::{Error, Read};
 
-use i
+use image::{GenericImageView, Luma};
+use nalgebra::{Affine2, Point2, SimilarityMatrix2};
+use pixelutil_image::get_pixel;
+
+use super::delta::ShaperDelta;
+use super::tree::ShaperTree;
+
+#[derive(Debug, Clone)]
+pub struct ShaperForest {
+    deltas: Vec<ShaperDelta>,
+    t
