7674b4d0f26e14a681343e20c279eec0876ea238
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-11-01 23:00:44 +0000

    spell error

--- src/shape/mod.rs
+++ src/shape/mod.rs
@@ -1,3 +1,17 @@
 mod delta;
 mod forest;
-mod tree
+mod tree;
+mod utils;
+
+use std::{
+    fmt::Debug,
+    io::{Error, ErrorKind, Read},
+};
+
+use image::{GenericImageView, Luma};
+use imageproc::rect::Rect;
+use nalgebra::{Affine2, DimName, Matrix3, Point2, SimilarityMatrix2, U2};
+
+use forest::ShaperForest;
+
+/// Implements object alignment using an ensemble of regression trees.
