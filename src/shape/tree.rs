22cf46364de9e65ad0e0d5ddf3575cd9eee98386
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-12-26 19:48:55 +0000

    Minor code refactoring.

--- src/shape/tree.rs
+++ src/shape/tree.rs
@@ -9,4 +9,19 @@ use super::utils::read_shape;
 #[derive(Debug, Clone)]
 pub struct ShaperTree {
     nodes: Vec<ThresholdNode>,
-    shifts: V
+    shifts: Vec<Vec<Vector2<f32>>>,
+}
+
+impl ShaperTree {
+    #[cfg(test)]
+    pub fn nodes(&self) -> usize {
+        self.nodes.len()
+    }
+
+    #[cfg(test)]
+    pub fn shifts(&self) -> usize {
+        self.shifts.len()
+    }
+
+    #[inline]
+    pub fn node(&self, index: usize) -> &Threshold
