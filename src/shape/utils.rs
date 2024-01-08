f8c274d673d36ff831161f6b3167ded6e4e899d0
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-01-08 06:29:02 +0000

    Update .github/config.yml

--- src/shape/utils.rs
+++ src/shape/utils.rs
@@ -1,3 +1,12 @@
 use std::io::{Error, Read};
 
-use nalgebra::{allocator::Allocator, DefaultAllocator, Dim, Dyn, OMat
+use nalgebra::{allocator::Allocator, DefaultAllocator, Dim, Dyn, OMatrix, UninitMatrix, U2};
+
+#[inline]
+pub fn read_shape<R: Read>(mut reader: R, size: usize) -> Result<OMatrix<f32, U2, Dyn>, Error>
+where
+    DefaultAllocator: Allocator<f32, U2, Dyn>,
+{
+    let mut m = UninitMatrix::<f32, U2, Dyn>::uninit(U2, Dyn::from_usize(size));
+
+    let mut bu
