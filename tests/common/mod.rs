7139e40d30a0a24b6967cb4bbe52ab36354ee689
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-01-11 22:24:06 +0000

    Another README update

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -12,4 +12,19 @@ use rand_xoshiro::Xoroshiro128PlusPlus;
 
 use pico_detect::{
     clusterize::Clusterizer, multiscale::Multiscaler, DetectMultiscale, Detector,
-    Localiz
+    Localizer, Padding, Shaper, Square, Target, LocalizePerturbate,
+};
+
+#[fixture]
+pub fn test_image_path() -> PathBuf {
+    "./assets/test.png".into()
+}
+
+#[fixture]
+pub fn detector_path() -> PathBuf {
+    "./models/face.detector.bin".into()
+}
+
+#[fixture]
+pub fn localizer_path() -> PathBuf {
+    "./mode
