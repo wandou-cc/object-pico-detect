1539a5f0ce3690efbd88992ebcc9cb7ffc1f3d4a
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-01-12 17:59:48 +0000

    correzione

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -41,3 +41,14 @@ pub fn test_image(test_image_path: PathBuf) -> image::GrayImage {
 }
 
 #[fixture]
+pub fn detector(detector_path: PathBuf) -> Detector {
+    Detector::load(file(detector_path)).unwrap()
+}
+
+#[fixture]
+pub fn localizer(localizer_path: PathBuf) -> Localizer {
+    Localizer::load(file(localizer_path)).unwrap()
+}
+
+#[fixture]
+pub fn shaper(shaper_path: PathBuf) -> Sha
