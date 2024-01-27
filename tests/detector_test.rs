d95680bd585617b4564de06839ba961b3332615f
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-01-27 12:47:11 +0000

    iroiro

--- tests/detector_test.rs
+++ tests/detector_test.rs
@@ -2,4 +2,12 @@ mod common;
 
 use approx::assert_abs_diff_eq;
 use image::GrayImage;
-use rs
+use rstest::rstest;
+
+use pico_detect::{DetectMultiscale, Detector, Square, Target};
+
+use common::{classify_case, detect_multiscale, detect_multiscale_case, detector};
+
+#[rstest]
+fn test_detector_classify(detector: Detector, classify_case: (GrayImage, Square, Option<f32>)) {
+    let (image,
